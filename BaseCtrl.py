8b847b3652d65e7ff6ec3411660ab92338074a5e
Venugopal Ibharampur <venug_pal@hotmail.com>
2019-03-18 23:28:18 +0530

    Update BaseCtrl.py
    Updating with new changes

--- BaseCtrl.py
+++ BaseCtrl.py
@@ -16,24 +16,25 @@ class Team(db.Model):
     __tablename__ = 'Team' 
     team_id = db.Column(db.Integer, primary_key=True)
     team_name = db.Column(db.String(80))
-    team_image = db.Column(db.String(120))
+    team_image = db.Column(db.BLOB)
 
     def __repr__(self):
-        return "<Team(team_name='%s', team_id='%s)>" % (
-                                self.team_name, self.team_id)
+        return "<Team(team_name='%s', team_id='%s, team_image='%s')>" % (
+                                self.team_name, self.team_id, self.team_image)
 
 
 class Player(db.Model):
     """docstring for Player"""
-    __tablename__ = 'Player'
+    __tablename__ = 'player'
     player_id = db.Column(db.Integer, primary_key=True)
     player_fname = db.Column(db.String(80))
     player_lname = db.Column(db.String(80))
+    player_image = db.Column(db.BLOB)
     team_id = db.Column(db.Integer, db.ForeignKey('Team.team_id'))
 
     def __repr__(self):
-        return "<Team(player_name='%s', player_id='%s, team_id='%s)>" % (
-                                self.player_name, self.player_id, self.team_id)
+        return "<Team(player_fname='%s', player_id='%s, team_id='%s)>" % (
+                                self.player_fname, self.player_id, self.team_id)
         
 @app.route('/')
 def home():
@@ -57,10 +58,18 @@ def do_admin_login():
 def add_team_player():
     if request.form['add_team_play'] == 'Add Team':
         return render_template('addteam.html')
-    else:
+    elif request.form['add_player_play'] == 'Add Player':
         teams = get_team()
         return render_template('addplayer.html', teams=teams)
+    else:
+        import pdb; pdb.set_trace()
+        players = Player.query.all()
+        return render_template('viewplayers.html', players=players)
 
+# @app.route('/viewplayer', methos=['GET'])
+# def view_players():
+#     players = Team.query.all()
+#     return render_template('viewplayers.html', players=players)
 
 @app.route('/addteam', methods=['POST', 'GET'])
 def add_team():
@@ -68,7 +77,9 @@ def add_team():
         result = request.form
         team = Team.query.filter_by(team_name=result['team_name']).first()
         if not team:
-            team1 = Team(team_name=result['team_name'])
+            with open(result['file'], 'rb') as file:
+                Image_binaryData = file.read()
+            team1 = Team(team_name=result['team_name'],team_image=Image_binaryData)
             db.session.add(team1)
             db.session.commit()
             flash(result['team_name'] + ' is added successfully')
@@ -83,21 +94,48 @@ def add_team():
 def player_information():
     if request.method == 'POST':
         result = request.form
-        import pdb; pdb.set_trace()
-        team = Player(player_fname=result['player_first_name'], player_lname=result['player_last_name'], team_id=result['team_selected'])
-        db.session.add(team)
+        player = Player(player_fname=result['player_first_name'], player_lname=result['player_last_name'], team_id=result['team_selected'])
+        db.session.add(player)
         db.session.commit()
-        return render_template('team-players.html')
+        teams = get_team()
+        if teams:
+            return render_template('team-players.html', teams=teams)
+        else:
+            return render_template('team-players.html')
 
 def get_team():
     teams = Team.query.all()
     return teams
 
-@app.route('/detail/<int:team_id>', methods=['POST', 'GET'])
+
+@app.route('/edit_team/<int:team_id>', methods=['POST', 'GET', 'PUT'])
 def team_edit(team_id):
+    if request.method == 'GET':
+        team = Team.query.filter_by(team_id=team_id).one()
+        return render_template('edit_team.html', team=team)
+
+
+@app.route('/update_team', methods=['POST', 'GET'])
+def updateteam():
     if request.method == 'POST':
-        import pdb; pdb.set_trace()
-        return render_template('addteam.html')
+        result = request.form
+        team = Team.query.filter_by(team_id=result.get('team_id')).one()
+        team.team_name = result.get('team_name')
+        db.session.commit()
+        teams = get_team()
+        if teams:
+            return render_template('team-players.html', teams=teams)
+
+@app.route('/deleteTeam/<int:team_id>', methods=['POST', 'GET'])
+def delete_team(team_id):
+    if request.method == 'GET':
+        Team.query.filter_by(team_id=team_id).delete()
+        db.session.commit()
+        teams = get_team()
+        if teams:
+            return render_template('team-players.html', teams=teams)
+        else:
+            return render_template('team-players.html')
 
 app.secret_key = os.urandom(24)
 app.run()
